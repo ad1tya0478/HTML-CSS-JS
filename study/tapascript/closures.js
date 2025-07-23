a08fb46bd5c22323735df1d98fa74bfdaa84a247
ad1tya0478 <aditya.50478@gmail.com>
2025-07-23 23:02:30 +0530

    Started Learning Closures

---
+++ study/tapascript/closures.js
@@ -0,0 +1,5 @@
+// Closure - A closure is a function that retains access to its lexical scope, even when that function is executed outside of that scope.
+// a closure allows a function to access a varibale from its outer scope 
+
+
+
